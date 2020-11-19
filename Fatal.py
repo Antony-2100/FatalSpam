e77812ea8e92ad1e0bd304801989849df2ee857b
KamZ0 <74688013+KamZ0@users.noreply.github.com>
2020-11-19 17:13:59 +0700

    Update Fatal.py

--- Fatal.py
+++ Fatal.py
@@ -3,10 +3,25 @@ import threading, os, sys, json, time
 from bs4 import BeautifulSoup as bs
 import requests as ru
 from fake_useragent import UserAgent
-from warn.warn import *
-from etc.bener import *
-from etc.ua import *
-from etc.loding import *
+# -*- coding: utf-8 -*-
+def bnr():
+ print (f"""
+{ken}    ______   _______     _       ____    ____  
+{kon}  .' ____ \ |_   __ \   / \     |_   \  /   _| 
+{ken}  | (___ \_|  | |__) | / _ \      |   \/   |   
+{kon}   _.____`.   |  ___/ / ___ \     | |\  /| |   
+{ken}  | \____) | _| |_  _/ /   \ \_  _| |_\/_| |_  
+{kon}   \______.'|_____||____| |____||_____||_____| 
+       {im}╔═╗┬  ┬{anj}    {im}╔═╗┌─┐┬─┐  {anj}╔═╗┌┐┌┌─┐
+       {anj}╠═╣│  │{im}    {anj}╠╣ │ │├┬┘  {im}║ ║│││├┤ 
+       {im}╩ ╩┴─┘┴─┘{anj}  {im}╚  └─┘┴└─  {anj}╚═╝┘└┘└─┘
+{im}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+{kun}[$] Author : {kin}Baiyu Poetra Tama
+{kun}[$] Nama SC : {kin}Spam All For One
+{kun}[$] Github : {kin}https://github.com/KamZ0
+{kun}[$] Youtube : {kin}Nothing
+{im}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••""")
+
 class main:
  def main():
   hyu = str(input(f"{kun}[{kan}•{kun}] {im}Masukkan pilihan : "))
